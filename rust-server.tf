f1e24b544501b0d08a29ab6922ada138898de5bd
TacoSauceDev <86977366+TacoSauceDev@users.noreply.github.com>
2021-07-08 22:26:25 -0600

    Update rust-server.tf

--- rust-server.tf
+++ rust-server.tf
@@ -5,7 +5,7 @@ resource "digitalocean_droplet" "rust_pve_dev" {
     image = "ubuntu-20-04-x64"
     name = "Rust-PVE-Dev"
     region = "nyc1"
-    size = "s-1vcpu-1gb"
+    size = "s-4vcpu-8gb"
     private_networking = true
     user_data = data.template_file.user_data.rendered
     lifecycle {
